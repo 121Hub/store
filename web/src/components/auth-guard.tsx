4d292a8d91c217d8648f8f98c0ca49ccef2cbaaa
Mike <michaelokolo62@yahoo.com>
2025-11-01 03:06:34 +0000

    solve next router error on build

--- web/src/components/auth-guard.tsx
+++ web/src/components/auth-guard.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useAuth } from '../context/AuthContext';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 export default function AuthGuard({ children }: { children: React.ReactNode }) {
